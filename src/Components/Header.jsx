fd064530da146b7777b29e5bd4fef2c3e15598af
Aniket <aniketsen521@gmail.com>
2025-03-15 22:20:00 +0530

    header logo path update

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -35,7 +35,7 @@ const Header = () => {
           
           {/* Logo & Brand Name */}
           <div className="flex items-center font-bold font-serif italic text-2xl header-logo">
-            <img src="public\logo.png" alt="" className="h-20" />
+            <img src="./logo.png" alt="" className="h-20" />
             <h1 className="text-yellow-300">
               <span className="text-pink-600">C</span>
               <span className="text-violet-400">ode</span> 
