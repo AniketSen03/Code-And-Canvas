81f4d077c33c29c1e8dcb80ef28f03c9c10fe222
Aniket <aniketsen521@gmail.com>
2025-05-26 19:42:55 +0530

    web update

--- src/Components/Web.jsx
+++ src/Components/Web.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 
+const web = [
+  {
+    img: '/screen.jpg',
+    alt: 'Screen design '
+  },
+  {
+    img: '/web  page design.png',
+    alt: 'Web page design '
+  }
+];
+
 const Web = () => {
   return (
-    <>
-    <img src="/1.jpg" alt="" />
-    </>
-  )
+    <div className="flex flex-wrap justify-center gap-8 p-6">
+      {web.map((item, idx) => (
+        <img
+          key={idx}
+          src={item.img}
+          alt={item.alt}
+          className="rounded-xl shadow-lg max-w-md w-full object-cover transition-transform duration-200 hover:scale-105"
+        />
+      ))}
+    </div>
+  );
 }
 
 export default Web
